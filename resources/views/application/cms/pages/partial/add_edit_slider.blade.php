698d0522fc4218289d25432bebbb494977d2c550
albertinho88 <Alberto@DESKTOP-2MCN5DG>
2017-04-24 22:51:04 -0500

    website cms

--- resources/views/application/cms/pages/partial/add_edit_slider.blade.php
+++ resources/views/application/cms/pages/partial/add_edit_slider.blade.php
@@ -10,32 +10,46 @@
     
     <div class="EmptyBox10"></div>
     
-    <div class="ui-g-12 ui-md-6 ui-lg-4">
-        <div class="ui-g card overview-box overview-box-1 " style="background-color: #BDBDBD;">  
-            test
+    @foreach ($active_sliders as $asl)
+        <div class="ui-g-12 ui-md-6 ui-lg-6">
+            <div class="ui-g card overview-box overview-box-1 " style="background-color: #BDBDBD;">                                  
+                <div class="callbacks_container">
+                    <ul class="rslides" id="{{ $asl->name }}">
+                        @foreach($asl->slider_images as $slide)
+                            <li>
+                              <img src="{{ asset($slide->image_path) }}" alt="">                              
+                              <p class="caption">{{ $slide->caption }}</p>
+                            </li>
+                        @endforeach                      
+                    </ul>
+                  </div>
+            </div>
         </div>
-    </div>
-    <div class="ui-g-12 ui-md-6 ui-lg-4">
-        <div class="ui-g card overview-box overview-box-1 " style="background-color: #BDBDBD;">  
-            test
-        </div>
-    </div>
-    <div class="ui-g-12 ui-md-6 ui-lg-4">
-        <div class="ui-g card overview-box overview-box-1 " style="background-color: #BDBDBD;">  
-            test
-        </div>
-    </div>
-    
-    <div class="ui-g-12 ui-md-6 ui-lg-4">
-        <div class="ui-g card overview-box overview-box-1 " style="background-color: #BDBDBD;">  
-            test
-        </div>
-    </div>
-    <div class="ui-g-12 ui-md-6 ui-lg-4">
-        <div class="ui-g card overview-box overview-box-1 " style="background-color: #BDBDBD;">  
-            test
-        </div>
-    </div>
+        <script type="text/javascript">
+            $("#{{ $asl->name }}").responsiveSlides({
+                auto: <?php echo $asl->animate_automatically?'true':'false'; ?>,             // Boolean: Animate automatically, true or false
+                speed: {{ $asl->transition_speed }},            // Integer: Speed of the transition, in milliseconds
+                timeout: {{ $asl->time_between_transition }},          // Integer: Time between slide transitions, in milliseconds
+                pager: <?php echo $asl->show_pager?'true':'false'; ?>,           // Boolean: Show pager, true or false
+                nav: false,             // Boolean: Show navigation, true or false
+                random: false,          // Boolean: Randomize the order of the slides, true or false
+                pause: false,           // Boolean: Pause on hover, true or false
+                pauseControls: true,    // Boolean: Pause when hovering controls, true or false
+                prevText: "Previous",   // String: Text for the "previous" button
+                nextText: "Next",       // String: Text for the "next" button
+                maxwidth: "",           // Integer: Max-width of the slideshow, in pixels
+                navContainer: "",       // Selector: Where controls should be appended to, default is after the 'ul'
+                manualControls: "",     // Selector: Declare custom pager navigation
+                namespace: "callbacks",   // String: Change the default namespace used
+                before: function () {
+                    $('.events').append("<li>before event fired.</li>");
+                },
+                after: function () {
+                    $('.events').append("<li>after event fired.</li>");
+                }
+              });
+        </script>
+    @endforeach             
     
     <div class="EmptyBox10"></div>
     
